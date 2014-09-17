ab0ada3ae9de88d8880017bf87d7ee6cedc2f9d7
Sid Sharma <sid@zineone.com>
2014-09-17 10:31:33 -0700

    Initial Commit

---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/SSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSAppDelegate.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Saurav Sharma on 9/17/14.
+//  Copyright (c) 2014 ZineOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/SSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSViewController.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Saurav Sharma on 9/17/14.
+//  Copyright (c) 2014 ZineOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSViewController : UIViewController
+
+@end
